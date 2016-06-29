91351b441f5ec88dbe11ff01005066fc14831bb6
Enzo <diroberto.v@stesi.eu>
2016-06-29 10:58:40 +0200

    StesiColumn into separated file

--- StesiTable/StesiTable.php
+++ StesiTable/StesiTable.php
@@ -5,7 +5,7 @@ namespace Stesi\StesiTable;
 
 use PFBC\Form;
 use PFBC;
-
+use StesiColumn;
 
 /**
  * Class that manage Datatable
@@ -206,23 +206,22 @@ class StesiTable {
 		$tableColums = $this->getColumns ();
 		$th="<tr>";
 		foreach ( $tableColums as $column ) {
-			$th .= "<th  data-type='".(
-					array_flip((new \ReflectionClass("Stesi\StesiTable\StesiColumnType"))->getConstants())[$column->getColumnType()]
-					)."'>" . $column->getColumnDescription () . "</th>";
-		
+			$th .= "<th>" . $column->getColumnDescription () . "</th>";
 		}
 		$th.="</tr>";
 		
 		$table .= "<thead>";
-		$table .= $th."</thead>";
-		$table .= "<tfoot>";
-		$table .= $th."</tfoot></table>";
-		$table .= "<script>
-				";
+		$table .= $th."</thead></table>";
+		$table .= $this->createJsScript($ajaxCallBack);
+		return $table;
+	}
+	
+	private function createJsScript($ajaxCallBack){
+		$tableColums = $this->getColumns ();
+		
+		$table="<script>";
 		$table .= '
-				
-				 $.fn.serializeForm = function() {
-
+				$.fn.serializeForm = function() {
     if ( this.length < 1) { 
       return false; 
     }
@@ -323,20 +322,6 @@ class StesiTable {
 			            }
 			          
 			        });
-			        		
-				   //initFooterEvent();
-				   api.columns().every( function () {
-					var column=this;
-	               column.data().unique().sort().each( function ( d, j ) {
-	             			if(d){
-							if(column.search() == d){
-						        $( "select", column.footer() ).append( "<option value=\""+d+"\" selected=\"selected\">"+d+"</option>" )
-						    }else {
-		                        $( "select", column.footer() ).append( "<option value=\""+d+"\">"+d+"</option>" )
-						    }
-	           				}
-		                } );
-				   });
 			},			        		
 			createdRow: function(row,data,index){';
 		foreach ( $tableColums as $column ) {
@@ -370,37 +355,8 @@ class StesiTable {
 		$table .= '}
 	});
 	
-			 // Restore state
-        var state = datatable.state.loaded();
-        if (state) {
-            datatable.columns().eq(0).each(function (colIdx) {
-                var colSearch = state.columns[colIdx].search;
- 
-                if (colSearch.search) {
-                    $("input", datatable.column(colIdx).footer()).val(colSearch.search);
-                }
-            });
- 
-            //datatable.draw();//Francesco per doppia chiamata
-        }	
+			
 				';
-		
-
-// 		$("#' . $this->id . '_form input").unbind();
-// 		$("#' .$this->id . '_form input").bind("keyup", function(e) {
-// 			if(e.keyCode == 13 || (e.keyCode==8 && this.value=="")) {
-		
-// 				var t=datatable;
-// 				$("#' .$this->id . '_form input").each(function(index){
-// 					if(this.value!=""){
-// 						t=t.column(($(this)).attr("name")+":name");
-// 						t=t.search(this.value);
-// 					}
-// 				});
-// 					console.log(t.draw());
-// 			}
-// 		});
-
 		
 		
 		/*
@@ -425,251 +381,33 @@ class StesiTable {
 	             						dataTableId: '" . $this->id . "'
 									}
 								});
-	             				initFooterEvent();
 	             	}";
 			}
 			$table .= "
 			});";
 		}
-		$table .= "		
-				var initFooterEvent=function(){
+		$table .= "
+					 // Restore state
+        var state = datatable.state.loaded();
+        if (state) {
+            datatable.columns().eq(0).each(function (colIdx) {
+                var colSearch = state.columns[colIdx].search;
+ 
+                if (colSearch.search) {
+                    $('input', datatable.column(colIdx).footer()).val(colSearch.search);
+                }
+            });
+        }	
 				
-				   // Apply the search
-	    			datatable.columns().every( function () {
-		        		var that = this;
-	             		var column=this;
-		 				$( 'input', this.footer() ).unbind();
-		        		$( 'input', this.footer() ).on( 'keyup change', function (e) {
-
-		              		if(e.keyCode == 13 || (e.keyCode==8 && this.value=='')) {
-		                		that.search( this.value )
-		                    	.draw();
-		            			}
-		             	});
-	             		$( 'select', this.footer() ).unbind();
-	             	   $( 'select', this.footer() ).on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
- 
-                        column
-                            .search( val ? val : '', true, false )
-                            .draw();
-                    	} );
- 
-		              
-						});
-				}
 				</script>";
 		return $table;
 	}
+	
 	
 	public function renderForm(){
 		if($this->form){
 			return $this->form->render(true);
 		}
 		return "";
-	}
-}
-class StesiColumn {
-	private $columnName;
-	private $columnDescription;
-	private $globalSearcheable;
-	private $stesiColumnStyles;
-	private $columnType;
-	private $stesiColumnValue;
-	private $options;
-	private $properties;
-	
-	
-	/**
-	 * 
-	 * @param string $columnName name of the column
-	 * @param string $columnDescription description of the column to use on the header table
-	 * @param boolean $globalSearcheable if true, the column is used in global search
-	 */
-	function __construct($columnName, $columnDescription = null, $globalSearcheable = 0) {
-		$this->columnName = $columnName;
-		$this->globalSearcheable = $globalSearcheable;
-		$this->stesiColumnStyles = array ();
-		if ($columnDescription)
-			$this->columnDescription = $columnDescription;
-		else
-			$this->columnDescription = $columnName;
-		$this->columnType=StesiColumnType::TextBox;
-		$this->options=array();
-		$this->properties=array();
-	}
-	/**
-	 * 
-	 * @param boolean $dot if true, dot is removed from column name
-	 */
-	public function getColumnName($dot = true) {
-		if ($dot)
-			return $this->columnName;
-		else
-			return str_replace ( ".", "", $this->columnName );
-	}
-	/**
-	 * Define type of stesiColumn
-	 * @param $columnType 
-	 */	
-	public function setColumnType($columnType){
-		$this->columnType=$columnType;
-		return $this;
-	}
-	/**
-	 * @return StesiColumnType $columnType
-	 */
-	public function getColumnType(){
-		return $this->columnType;
-	}
-	
-	public function getColumnDescription() {
-		return $this->columnDescription;
-	}
-	/**
-	 * 
-	 * @param string $columnDescription
-	 */
-	public function setColumnDescription($columnDescription) {
-		$this->columnDescription = $columnDescription;
-		return $this;
-	}
-
-	/**
-	 * Add an array of option used in PFBC column
-	 * @param unknown $options
-	 */
-	public function addOptions($options){
-		$this->options=$options;
-	}
-	
-	public function isGlobalSerchable() {
-		return $this->globalSearcheable;
-	}
-	public function getColumnStyles() {
-		return $this->stesiColumnStyles;
-	}
-	/**
-	 * 
-	 * @param string $conditionOperator
-	 * @param string $value
-	 * @return StesiColumnStyle $stesiColumnStyle
-	 */
-	public function addColumnStyle($conditionOperator, $value) {
-		$stesiColumnStyle = new StesiColumnStyle ( $conditionOperator, $value );
-		$this->stesiColumnStyles [] = $stesiColumnStyle;
-		return $stesiColumnStyle;
-	}
-	
-	/**
-	 * stesiColumnValue return array of default value
-	 * @return array
-	 */
-	public function getStesiColumnValue(){
-		return $this->stesiColumnValue;
-	}
-	
-	/**
-	 * Take in input a list of options to use in a select\multiselect column type
-	 * @param array $options
-	 */
-	public function setOptions(array $options){
-		$this->options=$options;
-		return $this;
-	}
-	/**
-	 * @return array $options
-	 */
-	public function getOptions(){
-		return $this->options;
-	}
-	
-	/**
-	 * Take in input a list of properties to configure column
-	 * @param array $properties
-	 */
-	public function setProperties(array $properties){
-		$this->properties=$properties;
-		return $this;
-	}
-	/**
-	 * @return array $properties
-	 */
-	public function getProperties(){
-		return $this->properties;
-	}
-	
-}
-/**
- * 
- * Define the type of column
- * @author Vincenzo
- *
- */	
-class StesiColumnType
-{
-	const TextBox  = 0;
-	const Select = 1;
-	const Number=3;
-	const Date=4;
-	const DateTime=5;
-	const TextArea=6;
-}
-/**
- * Define style of the column of the DataTable
- *  */
-class StesiColumnStyle {
-	private $operator;
-	private $value;
-	private $classes;
-	private $css;
-	
-	/**
-	 * 
-	 * @param string $conditionOperator operator used to apply the condition Example '=','>','<'
-	 * @param string $value value of the expression example 1000 
-	 */
-	function __construct($conditionOperator, $value) {
-		$this->operator = ($conditionOperator == "=" ? "==" : $conditionOperator);
-		$this->value = $value;
-		$this->css = array ();
-		$this->classes = array ();
-	}
-	
-	/**
-	 * Add css style to the column
-	 * @param string $propertyName
-	 * @param string $value
-	 * @return \Stesi\StesiTable\StesiColumnStyle
-	 */
-	function addCss($propertyName, $value) {
-		$this->css [] = array (
-				"propertyName" => $propertyName,
-				"value" => $value 
-		);
-		return $this;
-	}
-	/**
-	 * Add class to the column
-	 * @param string $className
-	 * @return \Stesi\StesiTable\StesiColumnStyle
-	 */
-	function addClass($className) {
-		$this->classes [] = $className;
-		return $this;
-	}
-	function getClasses() {
-		return $this->classes;
-	}
-	function getCss() {
-		return $this->css;
-	}
-	function getConditionOperator() {
-		return $this->operator;
-	}
-	function getValue() {
-		return $this->value;
 	}
 }
